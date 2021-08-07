aa03f9f4e288fa5419a92cf3c3e832d07a6b5178
paner <toshi23masa@gmail.com>
2021-08-07 23:42:06 +0900

    3rd commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from "react"
 import "../styles/layout.css"
 import Header from "./header"
 import Footer from "./footer"
+import "katex/dist/katex.min.css"
 
 const Layout = ({ children }) => {
     return (
